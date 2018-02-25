fa07ebfcafedde58421e2d2a0ef2469ec1432f4d
Luke Egan <eganlu@tcd.ie>
2018-02-25 00:56:53 +0000

    Un-fucked the www file

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var flash = require('connect-flash');
-var passport = require('passport');
-var session = require('express-session');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const passport = require('passport');
+const session = require('express-session');
 
-var index = require('./routes/index');
-var home = require('./routes/home');
+const index = require('./routes/index');
+const home = require('./routes/home');
 
-var app = express();
+const app = express();
 
 //Import the mongoose module
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 //Set up default mongoose connection
-var mongoDB = 'mongodb://localhost:27017/edwardiandoordb';
+const mongoDB = 'mongodb://localhost:27017/edwardiandoordb';
 mongoose.connect(mongoDB);
 //Get the default connection
 mongoose.connection.on('connected', function () {
@@ -33,6 +33,7 @@ app.use(session({ secret: 'thisisthesecret',
     resave: true,
     saveUninitialized: true
 }));
+
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
@@ -41,7 +42,6 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -54,7 +54,7 @@ app.use('/home',home);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
---
+++ bin/www.js
@@ -0,0 +1,26 @@
+const app = require('../app');
+const debug = require('debug')('edwardiandoor:server');
+const https = require('https');
+const fs = require('fs');
+const http = require('http');
+
+const privateKey = fs.readFileSync('../private.key');
+const certificate = fs.readFileSync('../certificate.crt');
+const DEBUG = true;
+
+if (DEBUG) {
+    app.set('port', 3000);
+    http.createServer(app).listen(3000);
+} else {
+    app.set('port', 443);
+    https.createServer({
+        key: privateKey,
+        cert: certificate
+    }, app).listen(443);
+    http.createServer(function (req, res) {
+        res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+        res.end();
+    }).listen(80);
+}
+
+
