0c0e2bbf0037904e77e77e9bedd75d26e1100e88
Ben Stratford <stratfob@tcd.ie>
2018-02-27 01:43:21 +0000

    Leaderboard is now awesome and fixed things like case sensitivity of login emails

--- routes/index.js
+++ routes/index.js
@@ -11,17 +11,19 @@ router.get('/', function (req, res) {
 
 /* GET login page. */
 router.get('/login', function (req, res) {
-    res.render('login', {message: req.flash('loginMessage')});
+    res.render('login', {loginMessage: req.flash('loginMessage'), err: req.flash('err'), succ: req.flash('succ')});
 });
 
-router.get('/leaderboards', isLoggedIn, function(req,res){
+router.get('/leaderboards/:pageNum', isLoggedIn, function(req,res){
 	userMapper.allUsers(function(error, result){
 		if(error){
 			req.flash('err', 'An error occured');
             res.redirect('/home');
 		}
 		else {
-            res.render('leaderboards', {user: req.user, result});
+			let numberOfPages = Math.floor(result.length/10);
+			if(result.length%20>0){numberOfPages+=1;}
+            res.render('leaderboards', {user: req.user, result, pageNumber:req.params.pageNum, numberOfPages});
         }
 	});
 });
@@ -31,7 +33,7 @@ router.get('/home', isLoggedIn, function(req, res) {
 });
 
 /* POST login details. */
-router.post('/login', passport.authenticate('local-login', {
+router.post('/login', lowercaseifyEmail, passport.authenticate('local-login', {
     successRedirect: '/home', // redirect to the secure profile section
     failureRedirect: '/login', // redirect back to the login page if there is an error
     failureFlash: true // allow flash messages
@@ -46,15 +48,18 @@ router.post('/register', function(req,res){
 	if (validUserParams(req.body)) {
 		if (req.body.inputPassword === req.body.inputConfirmPassword) {
 
-			userMapper.addUser(req.body.inputUsername, req.body.inputEmail, req.body.inputPassword,function(error, result) {
+			userMapper.addUser(req.body.inputUsername, req.body.inputEmail.toLowerCase(), req.body.inputPassword,function(error, result) {
 				if (!result) {
-					req.flash('err', 'User could not be created');
+					req.flash('err', 'Username/email is unavailable');
+                    res.redirect('/register');
 				} else if (error) {
 					req.flash('err', error);
+                    res.redirect('/register');
 				} else {
 					req.flash('succ', 'Successfully created account!');
+                    res.redirect('/login');
 				}
-				res.redirect('/login');
+
 			});
 		} else {
 			req.flash('err', 'Passwords do not match');
@@ -70,4 +75,8 @@ function validUserParams(body) {
     return (body.inputUsername && body.inputEmail && body.inputPassword && body.inputConfirmPassword);
 }
 
+function lowercaseifyEmail(req, res, next) {
+    req.body.email = req.body.email.toLowerCase();
+    return next();
+}
 module.exports = router;
